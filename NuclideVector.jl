8bedb42a27913fa00385cb4ca6ef4b456cf45607
Martin Klemann <martin.klemann@ewn-gmbh.de>
2015-11-04 15:29:29 +0100

    add solver

--- NuclideVector.jl
+++ NuclideVector.jl
@@ -2,7 +2,6 @@
 # load modules
 using SQLite: query, SQLiteDB
 using NamedArrays
-using FunctionalData
 using JuMP
 using Cbc
 using YAML
@@ -11,10 +10,7 @@ using YAML
 # settings
 
 const sf = "settings_yaml.txt";
-
 settings = YAML.load(open(sf));
-
-rel_nuclides = convert(Array{ASCIIString, 1}, settings["nuclides"]);
 
 #######################################################
 # helper functions
@@ -27,17 +23,17 @@ function arr2str(nn::Array{ASCIIString, 1})
 	return nn_str
 end
 
-function arr2sym(nn::Array{ASCIIString, 1})
-	nn_sym = Array(Symbol, length(nn))
-	for i=1:length(nn)
-		nn_sym[i] = Symbol(nn[i])
-	end
-	return nn_sym
-end
+# function arr2sym(nn::Array{ASCIIString, 1})
+# 	nn_sym = Array(Symbol, length(nn))
+# 	for i=1:length(nn)
+# 		nn_sym[i] = Symbol(nn[i])
+# 	end
+# 	return nn_sym
+# end
 
 function schema2arr(x::DataStreams.Data.Table)
 	y = Array(Any, x.schema.rows, x.schema.cols)
-	@simd for i=1:x.schema.cols
+	for i=1:x.schema.cols
 		y[:,i] = x.data[i].values
 	end
 	return y
@@ -86,7 +82,7 @@ function decay_correction(nvdb::SQLite.DB, nuclide_names::Array{ASCIIString, 1})
 		tday_raw = [Date(years[1], month_, day_):Dates.Year(1):Date(years[end], month_, day_);]' .- sample_view; # calculate difference in days
 		tday = NamedArray(convert(Array{Int64,2}, tday_raw), (NamedArrays.names(samples, 1), years),  ("samples", "years") )
 	else
-		tday = NamedArray(zeros(size(sample_view, 1), years[end] - years[1] + 1), (NamedArrays.names(samples, 1), years),  ("samples", "years") )
+		tday = NamedArray(zeros(size(sample_view, 1), length(years) ), (NamedArrays.names(samples, 1), years),  ("samples", "years") )
 	end
 	
 	samples_korr = NamedArray(Array(Float64, size(tday,1), size(samples,2), size(tday,2)), 
@@ -105,53 +101,128 @@ end
 #######################################################
 # relevant nuclides and calculations
 
-#specific = read_setting(:specific, sf)
-samples_korr = decay_correction(nvdb, nuclide_names);
-samples_part = NamedArray( samples_korr./sum(samples_korr,2), samples_korr.dicts, samples_korr.dimnames)
-clearance_val = read_db(nvdb, "clearance_val");
-
-eff = read_db(nvdb, "efficiency");
-eff_red = eff[:, rel_nuclides];
-
-f = NamedArray( 1./clearance_val, clearance_val.dicts, clearance_val.dimnames);
-f_red = f[:, rel_nuclides];
-
-A = NamedArray(Array(Float64, size(samples_korr,1), size(clearance_val,1), size(samples_korr,3)), 
-				(allnames(samples_korr)[1], allnames(clearance_val)[1], get_years()), 
-				("sample", "path", "years")); # path -> clearance path
-C = NamedArray(Array(Float64, size(samples_korr,1), size(eff,1), size(samples_korr,3)), 
-				(allnames(samples_korr)[1], allnames(eff)[1], get_years()), 
-				("sample", "path", "years")); # path -> fma / fmab
-
-for i in [years[1]:years[2];]
-    A[:,:,i] = samples_part[:,:,i] * f'
-    C[:,:,i] = samples_part[:,:,i] * eff' # sum of Co60-equiv. also contain non measureable nuclides
+function nuclide_parts(samples_korr::NamedArrays.NamedArray{Float64,3,Array{Float64,3},Tuple{Dict{Any,Int64},Dict{ASCIIString,Int64},Dict{Int64,Int64}}})
+	NamedArray( samples_korr./sum(samples_korr,2), samples_korr.dicts, samples_korr.dimnames)
 end
-a = 1./A;
+
+function nuclide_parts()
+	samples_korr = decay_correction(nvdb, nuclide_names)
+	NamedArray( samples_korr./sum(samples_korr,2), samples_korr.dicts, samples_korr.dimnames)
+end
+
+function calc_factors(samples_part::NamedArrays.NamedArray{Float64,3,Array{Float64,3},Tuple{Dict{Any,Int64},Dict{ASCIIString,Int64},Dict{Int64,Int64}}})
+	#specific = read_setting(:specific, sf)
+	clearance_val = read_db(nvdb, "clearance_val");
+
+	eff = read_db(nvdb, "efficiency");
+	eff_red = eff[:, rel_nuclides];
+
+	f = NamedArray( 1./clearance_val, clearance_val.dicts, clearance_val.dimnames);
+	f_red = f[:, rel_nuclides];
+
+	A = NamedArray(Array(Float64, size(samples_part,1), size(clearance_val,1), size(samples_part,3)), 
+					(allnames(samples_part)[1], allnames(clearance_val)[1], get_years()), 
+					("sample", "path", "years")); # path -> clearance path
+	C = NamedArray(Array(Float64, size(samples_part,1), size(eff,1), size(samples_part,3)), 
+					(allnames(samples_part)[1], allnames(eff)[1], get_years()), 
+					("sample", "path", "years")); # path -> fma / fmab
+	
+	(ft = f'; efft = eff');
+	for i in get_years()
+		A[:,:,i] = samples_part[:,:,i] * ft
+		C[:,:,i] = samples_part[:,:,i] * efft # sum of Co60-equiv. also contain non measureable nuclides
+	end
+	a = 1./A;
+
+	return a, C, f_red, eff_red
+end
 
 #######################################################
 # solve problem
 
 function get_nv()
-	for l in get_years() # year
-		 solve_nv(l)
+	global settings = YAML.load(open(sf));
+	global rel_nuclides = convert(Array{ASCIIString, 1}, settings["nuclides"]);
+	
+	a, C, f_red, eff_red = decay_correction(nvdb, nuclide_names) |> nuclide_parts |> calc_factors
+	nv = Array(Float64, size(f_red,2), size(a,3)-1)
+	for l = 1:length(get_years())-1 #in get_years() # year
+		 nv[:,l] = solve_nv(l, a, C, f_red, eff_red)
 	end
+	write_result(nv)
 end
 
-function solve_nv(l::Int)
+function determine_fmx()
+	if (settings["use_fmab"] == "fma")
+		fmx = settings["clearance_paths"][1]["fma"]
+		(p = 1; q = 1);
+	elseif (settings["use_fmab"] == "fmb")
+		fmx = settings["clearance_paths"][1]["fmb"]
+		(p = 2; q = 2);
+	elseif (settings["use_fmab"] == "fmab")
+		fmx = unique([settings["clearance_paths"][1]["fma"]; settings["clearance_paths"][1]["fmb"]])
+		(p = 1; q = 2);
+	else
+		# throw error
+	end
+	return fmx, p, q
+end
+
+function solve_nv{ T1<:NamedArrays.NamedArray{Float64,3,Array{Float64,3},Tuple{Dict{Any,Int64},Dict{ASCIIString,Int64},Dict{Int64,Int64}}},
+				   T2<:NamedArrays.NamedArray{Any,2,Array{Any,2},Tuple{Dict{ASCIIString,Int64},Dict{ASCIIString,Int64}}}}(
+				   l::Int, a::T1, C::T1, f_red::T2, eff_red::T2 )
 	m=Model(solver = CbcSolver());
-	@defVar(m, 0 <= x[1:length(rel_nuclides)] <= 10000, Int); #≤
+	@defVar(m, 0 <= x[1:length(rel_nuclides)] <= 10_000, Int); #≤
 	@setObjective(m, :Min, -x[ find(rel_nuclides .== "Co60")][1] 
 						   -x[ find(rel_nuclides .== "Cs137")][1] );
-	@addConstraint(m, sum(x) == 10000);
+	@addConstraint(m, sum(x) == 10_000);
 
-	
+	# add user specified constraints
 	for i in settings["constraints"]
 		y = parse(i)
 		y.args[1] = :(x[ $(find(rel_nuclides .== string(y.args[1]) ))  ])
-		y.args[3] *= 10000
+		y.args[3] *= 10_000
 		#@addConstraint(m, esc(y)) <---- HERE
-		#Expr(:macrocall,symbol("@addConstraint"),esc(m),esc(y)
+		#Expr(:macrocall,symbol("@addConstraint"), esc(m), y)
 	end
 	
+	# Co60-equiv for nv
+	@defExpr(Co60eqnv[p=1:2], sum{eff_red.array[p,i] * x[i], i=1:length(rel_nuclides); eff_red[p,i] != 0}) # ∑
+	
+	# determine fma / fmb / fmab
+	fmx, p, q = determine_fmx()
+	
+	for r = p:q
+		# lower bound
+		@addConstraint(m, constr[k=1:length(fmx), j=1:size(a,1), h=0:1], 
+							Co60eqnv[r]
+							<= C.array[j, r, l+h] * a.array[j,k,l+h] * sum{f_red.array[k,i] * x[i], i=1:length(rel_nuclides)} )
+		if settings["use_upper_bound"]
+		# upper bound
+			@addConstraint(m, constr[k=1:length(fmx), j=1:size(a,1), h=0:1], 
+								C.array[j, r, l+h] * a.array[j,k,l+h] * sum{f_red.array[k,i] * x[i], i=1:length(rel_nuclides)} 
+								<= settings["upper_bound"] * Co60eqnv[r] )
+		end
+	end
+	
+	#@addConstraint(m, fmaconstr[k in fmx, j in allnames(a)[1], h=0:1], lhs[1] <= C[j,1,l+h] * a[j,k,l+h] * sum{f_red[k,i] * x[i], i=1:length(rel_nuclides)} )
+
+	sstatus = solve(m, suppress_warnings=true);
+
+	if (sstatus == :Infeasible)
+		print_with_color(:red, string(get_years()[l]) * " no solution in given bounds\n")
+		return zeros(length(x))
+	elseif sstatus == :Optimal
+		print_with_color(:green, string(get_years()[l]) * " solution found.\n")
+		return round(getValue(x)./100, 2)
+	else
+		print("Something weird happen! sstatus = " * string(sstatus) *"\n")
+		return zeros(length(x))
+	end
+	
+end
+
+function write_result(nv::Array{Float64,2})
+	NamedArray( nv, (rel_nuclides, get_years()[1:end-1]), ("nuclides", "years"))
+	#writetable(nv_name * "/" * string(get_years()[l]) * "_" * clearance_paths * ".csv", nv, separator=';')
 end
